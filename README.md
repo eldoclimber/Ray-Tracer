f6af7992029ed6cb8994693d25be5ef5e78449ae
Eliot Augusto <eliot.augusto@gmail.com>
2023-04-17 23:38:20 -0600

    First image render of a ppm file.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Ray Tracer
+
+This is a program that explores the basics of ray tracing and utilizing a cartesian coordinate system. This is just exploration and there are probably more elegant solutions on GitHub.
--- Ray Tracer/Ray Tracer.cpp
+++ Ray Tracer/Ray Tracer.cpp
@@ -2,19 +2,55 @@
 //
 
 #include <iostream>
+#include <string>
+//#include <sstream>  
+#include <fstream>
+using namespace std;
+
+string filename = "logbook.ppm";
+streambuf *psbuf, *backup;
+ofstream filestr;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int nx = 200;
+    int ny = 100;
+    string space = " ";
+    filestr.open(filename);
+
+    // TODO: Code "copied" from internet examples. Investigate what rdbuf does exactly and explore how the output of the file can be printed to console as well as the text file.
+    backup = cout.rdbuf();
+    psbuf = filestr.rdbuf();
+    cout.rdbuf(psbuf);
+    cout << "P3\n" << nx << space << ny << "\n 255\n";
+    cout.rdbuf(backup);
+
+
+    for (int j = ny-1; j >= 0; j--)
+    {
+        for (int i = 0; i < nx; i++)
+        {
+            float r = float(i) / float(nx);
+            float g = float(j) / float(ny);
+            float b = 0.2;
+            int ir = int(255.99 * r);
+            int ig = int(255.99 * g);
+            int ib = int(255.99 * b);
+
+            // TODO: Code "copied" from internet examples. Investigate what rdbuf does exactly and explore how the output of the file can be printed to console as well as the text file.
+            backup = cout.rdbuf();
+            psbuf = filestr.rdbuf();
+            cout.rdbuf(psbuf);
+            cout << ir << space << ig << space << ib << "\n";
+            cout.rdbuf(backup);
+
+
+            // TODO: Explore how stringstream is used. StackExchange suggests it is not useful when used at scale due to memory issues.
+            //stringstream outputString;
+            //outputString << ir << space << ig << space << ib << "\n";
+        }
+    }
+    filestr.close();
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
